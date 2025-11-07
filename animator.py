b56be8aabeea854185a70abd12caf9d04d08cf06
arifwcma <arif.rahman@wcma.vic.gov.au>
2025-11-07 15:03:38 +1100

    done

---
+++ animator.py
@@ -0,0 +1,7 @@
+import os
+import imageio.v2 as imageio
+
+folder = r"dump"
+files = sorted(os.listdir(folder), key=lambda x: int(os.path.splitext(x)[0]))
+images = [imageio.imread(os.path.join(folder, f)) for f in files]
+imageio.mimsave("animation.gif", images, duration=0.1)
